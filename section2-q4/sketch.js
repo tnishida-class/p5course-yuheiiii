43e58ecd2b12828cae2ab70a4f78b8e2af916b3e
yuheiiii <yuheilions@outlook.jp>
2021-10-19 01:35:37 +0900

    section2-q4

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -8,7 +8,11 @@ function setup() {
   let d = height / 9; // 縞1本の太さ
 
   for(let i = 0; i < 9; i++){
-    // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
+    if(i % 2 == 0){
+      fill(blue);
+    }else{
+      fill(255);
+    }// BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
     rect(0, i * d, width, (i + 1) * d);
   }
 
@@ -16,6 +20,8 @@ function setup() {
   let size = d * 5;
   rect(0, 0, size, size);
 
+  noStroke();
   fill(255);
-  // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
-}
+  rect(40, 0, size / 5, size)// BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
+  rect(0, 40, size, size / 5)
+ }
