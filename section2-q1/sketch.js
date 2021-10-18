8df5c25b53683588593f8aa96c988027bf624200
yuheiiii <yuheilions@outlook.jp>
2021-10-19 01:34:56 +0900

    section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,14 @@
 // 小手調べ
 function setup(){
+  createCanvas(200, 200);
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+    if(i < 5){
+      stroke(0, 0, 255);
+    }else{
+      stroke(255, 0, 0);
+    }
+    noFill();
+    let x = 10 + i * 10;
+    ellipse(60, 60, x);// BLANK[1]
   }
 }
