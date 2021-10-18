cfbe3b3986c67ed6f6a935b765f95be57ac0788e
yuheiiii <yuheilions@outlook.jp>
2021-10-19 01:35:14 +0900

    section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,7 +5,17 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      let sum = i + j;
+      fill(sum % 2 == 0? 255 : 100)// BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      rect(i * size, j * size, size, size)
+      if(j < 3 && sum % 2 != 0){
+        fill(196, 0, 0);
+      }else if (j < 5) {
+        noFill();
+      }else if(j < 8 && sum % 2 != 0){
+        fill(0);
+      }
+      ellipse(i * size + 12, j * size + 12, size - 5);
     }
   }
 }
