0bcc25a70227681aa91b52acee3a9f41f662cb8b
yuheiiii <yuheilions@outlook.jp>
2021-11-16 10:33:35 +0900

    section5-1

--- section5-1/sketch.js
+++ section5-1/sketch.js
@@ -7,8 +7,18 @@ function setup(){
     let theta = TWO_PI * i / 12;
     let x = 100 + cos(theta) * 50;
     let y = 100 + sin(theta) * 50;
-    ellipse(x, y, 10);
+    star(x, y, 10);
   }
 }
 
 // ヒント：section5-2 にある star 関数をここにコピーして、 draw 内で ellipse の代わりに使おう
+function star(cx, cy, r){
+  beginShape();
+  for(var i = 0; i < 5; i++){
+    let theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    let x = cx + cos(theta) * r;
+    let y = cy + sin(theta) * r;
+    vertex(x,y);
+  }
+  endShape(CLOSE);
+}
